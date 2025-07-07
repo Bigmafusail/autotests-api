986fffa362bc422fb95bbdef18d9e15b7f1e5a74
Bigmafusail <rudov.dmitriy@gmail.com>
2025-07-07 17:01:17 +0400

    Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,3 +1,5 @@
+from functools import lru_cache  # Импортируем функцию для кеширования
+
 from httpx import Client
 from pydantic import BaseModel
 
@@ -5,11 +7,12 @@ from clients.authentication.authentication_client import get_authentication_clie
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
-class AuthenticationUserSchema(BaseModel):
+class AuthenticationUserSchema(BaseModel, frozen=True):  # Добавили параметр frozen=True
     email: str
     password: str
 
 
+@lru_cache(maxsize=None)  # Кешируем возвращаемое значение
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
 
@@ -21,4 +24,3 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
         base_url="http://localhost:8000",
         headers={"Authorization": f"Bearer {login_response.token.access_token}"}
     )
-
